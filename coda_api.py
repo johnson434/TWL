f71521126cc88cedf166b4440cdf890ee7d520e4
jonathan <ghkddlsrb96@gmail.com>
2024-01-24 16:58:46 +0900

    페이지 목록 조회 API 추가
    로컬에 api key 작성하면 동작함.

--- coda_api.py
+++ coda_api.py
@@ -1,5 +1,21 @@
 import sys
+import requests
+
+def get_list_pages(coda_api_key, doc_id):
+    headers = {'Authorization': 'Bearer {}'.format(coda_api_key)}
+    uri = f'https://coda.io/apis/v1/docs/{doc_id}/pages'
+    res = requests.get(uri, headers=headers).json()
+    return res
 
 args = sys.argv
-args = args[1:len(args)] 
+coda_api_key = args[0]
+doc_id = "OBsatQ1Yn9"
+
+print("args : ", args)
+
+list_pages = get_list_pages(coda_api_key=coda_api_key, doc_id=doc_id)
+for page in list_pages["items"]:
+    name = page["name"]
+    id = page["id"]
+    print(f"name : {name} id : {id}")
 
