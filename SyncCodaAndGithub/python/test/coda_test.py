f1d167cfd40f561ab7af0c2360af8afb175b34c9
jonathan <ghkddlsrb96@gmail.com>
2024-01-26 21:21:17 +0900

    테스트 코드 작성 시작

---
+++ SyncCodaAndGithub/python/test/coda_test.py
@@ -0,0 +1,24 @@
+import sys
+import unittest
+import main.coda_api as coda_api
+import main.data.coda_data as coda_data
+import os
+
+class TestCodaAPIEndToEnd(unittest.TestCase):
+    def setUp(self):
+        self.coda_api_key = coda_api_key = open("../../../token.txt", mode='r').readline()
+        self.doc_id = "OBsatQ1Yn9"
+
+    def test_list_pages(self):
+        # 토큰 및 쿼리 파람 설정
+        coda_api_key = self.coda_api_key
+        doc_id = self.doc_id
+        response = coda_api.listPages(coda_api_key=coda_api_key, doc_id=doc_id)
+        assert type(response) is coda_data.ListPagesResponse
+
+    def test_list_pages_with_wrong_app_key(self):
+        response = coda_api.listPages(coda_api_key="wrong_app_key", doc_id='OBsatQ1Yn9')
+        self.assertIsNone(response)
+
+if __name__ == '__main__':
+    unittest.main()
