3d5ecc4dd3a87fbf123d763ce068b1c8add4243c
jonathan <ghkddlsrb96@gmail.com>
2024-01-27 13:24:42 +0900

    end-to-end test작성
    실제 appkey를 통해 실행하는 api 테스트 작성

--- SyncCodaAndGithub/python/test/test_coda_api.py
+++ SyncCodaAndGithub/python/test/test_coda_api.py
@@ -9,12 +9,12 @@ from main.data import coda_data
 
 class TestCodaAPIEndToEnd(unittest.TestCase):
     def setUp(self):
-        self.coda_api_key = coda_api_key = open("../../../token.txt", mode='r').readline()
+        self.api_key = sys.argv[1]
         self.doc_id = "OBsatQ1Yn9"
 
     def test_list_pages(self):
         # 토큰 및 쿼리 파람 설정
-        coda_api_key = self.coda_api_key
+        coda_api_key = self.api_key
         doc_id = self.doc_id
         response = coda_api.listPages(coda_api_key=coda_api_key, doc_id=doc_id)
         assert type(response) is coda_data.ListPagesResponse
@@ -24,4 +24,6 @@ class TestCodaAPIEndToEnd(unittest.TestCase):
         self.assertIsNone(response)
 
 if __name__ == '__main__':
-    unittest.main()
+    if len(sys.argv) <= 1:
+        raise TypeError("주어진 매개 변수의 개수가 1개 이하입니다. 파일과 API 키를 넘겨주세요.")
+    unittest.main(argv=sys.argv[:1])  # Pass only the script name to unittest.main()
