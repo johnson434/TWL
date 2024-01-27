14eb2435e0a14af7405e947187a61138083f7c13
jonathan <ghkddlsrb96@gmail.com>
2024-01-27 17:50:48 +0900

    로깅 설정

--- SyncCodaAndGithub/python/main/api/coda_api.py
+++ SyncCodaAndGithub/python/main/api/coda_api.py
@@ -1,63 +1,81 @@
 import requests
 import main.data.coda_data as coda_data
+import logging
 from main.file.file_format import FileFormat
 
 BASE_URL = "https://coda.io/apis/v1/docs"
 
+logger = logging.getLogger("main")
+
 # https://coda.io/developers/apis/v1#tag/Pages/operation/listPages
 def listPages(coda_api_key, doc_id) -> coda_data.ListPagesResponse:
+    logger.debug('called')
+
     headers = {'Authorization': 'Bearer {}'.format(coda_api_key)}
     uri = f'{BASE_URL}/{doc_id}/pages'
+    logger.info(f'headers : {headers}\nuri : {uri}')
+
     res = requests.get(uri, headers=headers)
     responseJson = res.json()
+    logger.info(f'responseJson : {responseJson}')
     
     if res.status_code != 200 : 
         error = coda_data.Error.from_dict(responseJson)
+        logger.error(error)
         return None
     
     return coda_data.ListPagesResponse.from_dict(responseJson)
 
 # https://coda.io/developers/apis/v1#tag/Pages/operation/beginPageContentExport
 def export_page(coda_api_key, doc_id, page_id) -> coda_data.ExportPageResponse:
+    logger.debug("called")
+
     headers = {'Authorization': 'Bearer {}'.format(coda_api_key)}
     uri = f'{BASE_URL}/{doc_id}/pages/{page_id}/export'
     payload = { 'outputFormat' : 'markdown' }
+    logger.info(f'headers : {headers}\nuri : {uri}\npayload : {payload}')
+
     res = requests.post(uri, headers=headers, json=payload)
     responseJson = res.json()
-    print(f'res : {res}')
-    print(f'responseJson : {responseJson}')
-    print(f'responseCode : ${res.status_code}')
+    logger.info(f'responseJson : {responseJson}')
 
     if res.status_code != 202 : 
         error = coda_data.Error.from_dict(responseJson)
-        print(f'statusCode : {error.statusCode}\nstatusMessage : {error.statusMessage}\nmessage : {error.message}')
+        logger.error(error)
         return None
     
     return coda_data.ExportPageResponse.from_dict(responseJson)
 
 def get_downloadLink(coda_api_key, doc_id, page_id, request_id) -> bool:
+    logger.debug('called')
+
     headers = {'Authorization': 'Bearer {}'.format(coda_api_key)}
     uri = f'{BASE_URL}/{doc_id}/pages/{page_id}/export/{request_id}'
-    print(f'is_export_done : uri : {uri}')
+    logger.info(f'headers : {headers}\nuri : {uri}\n')
+
     res = requests.get(url=uri, headers=headers)
     responseJson = res.json()
-    print(f'isExportDone responseJson : {responseJson}')
+    logger.info(f'responseJson : {responseJson}')
 
     if res.status_code != 200 or responseJson.get('status') != 'complete':
         error = coda_data.Error.from_dict(responseJson)
-        print(f'statusCode : {error.statusCode}\nstatusMessage : {error.statusMessage}\nmessage : {error.message}')
+        logger.error(error)
         return None
     
     return responseJson.get('downloadLink')
 
 def downloadLink(link, file_name, file_format: FileFormat):
+    logger.debug('called')
+
     res = requests.get(link)
-    fileName = "./" + file_name
+    fullFileName = "./" + file_name
     if file_format == FileFormat.Markdown:
-        fileName += ".md"
+        fullFileName += ".md"
     elif file_format == FileFormat.HTML:
-        fileName += ".html"
-    SAMPLE_FILE = open(fileName, 'wb')
+        fullFileName += ".html"
+    logger.info(f'fileName : {fullFileName}')
+
+    SAMPLE_FILE = open(fullFileName, 'wb')
     SAMPLE_FILE.writelines(res)
 
 
--- SyncCodaAndGithub/python/main/entry_point.py
+++ SyncCodaAndGithub/python/main/entry_point.py
@@ -1,17 +1,37 @@
 from api import coda_api
+from log.custom_log import *
 import sys
 import time
 import queue
+import logging
 
 # todo : 제거해야됨
 # coda_api_key = open("../../token.txt", mode='r').readline()
 doc_id = "OBsatQ1Yn9"
 
-def main():
+# # 로그 세팅
+logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# 로깅 형식 설정
+formatter = MethodNameFormatter('%(asctime)s - %(levelname)s - %(method_name)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# 로깅 핸들러 추가
+logger.addHandler(console_handler)
+
+# 테스트 로그
+logger.debug("logger debug")
+
+def main(): 
+    logger.debug("start")
     # 토큰 및 쿼리 파람 설정
     args = sys.argv
     coda_api_key = args[1]
-    
+
     # 가능한 페이지 출력
     list_pages = coda_api.listPages(coda_api_key=coda_api_key, doc_id=doc_id)
 
